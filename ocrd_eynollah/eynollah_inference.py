0cd2c937e3760aae9c405d9bca6da50bef9e8569
Tuyen Le <lethikimtuyen87@gmail.com>
2026-03-12 17:47:19 +0100

    convert Eynollah inference results to ocrd page, in progress

--- ocrd_eynollah/eynollah_inference.py
+++ ocrd_eynollah/eynollah_inference.py
@@ -6,12 +6,60 @@ import json
 import tempfile
 import cv2
 import numpy as np
+from collections import defaultdict
+from skimage import measure
+from shapely.geometry import Polygon
 
 from ocrd import Processor, Workspace, OcrdPage, OcrdPageResult, OcrdPageResultImage
-from ocrd_models.ocrd_page import CoordsType, TextRegionType, ImageRegionType
+from ocrd_models.ocrd_page import (
+    CoordsType,
+    TextRegionType,
+    ImageRegionType,
+    AlternativeImageType,
+)
 from ocrd_utils import points_from_bbox, points_from_polygon
 from ocrd.decorators import ocrd_cli_options, ocrd_cli_wrap_processor
 from eynollah.training.inference import sbb_predict as EynollahInference
+
+
+# color coding for Eynollah inference results,
+# used in visualize_model_output() method of sbb_predict class
+# the below colors are extracted from kimlee87/eynollah.git@new_inference_color
+# the original colors from eynollah.git@main are commented for reference
+# colors here are in RGB, not BGR as in Eynollah visualize_model_output()
+eynollah_inference_colors = {
+    # (R, G, B): (label, region type, region label, subtype)
+    (255, 255, 255): (
+        "background",
+        None,
+        None,
+        None,
+    ),  # background, same color as original
+    (231, 76, 60): (
+        "image",
+        ImageRegionType,
+        "ImageRegion",
+        None,
+    ),  # image, orginally (0, 0, 255)
+    (52, 152, 219): (
+        "text",
+        TextRegionType,
+        "TextRegion",
+        None,
+    ),  # text, orginally (0, 125, 255)
+    (230, 126, 34): (
+        "heading",
+        TextRegionType,
+        "TextRegion",
+        "heading",
+    ),  # heading, orginally (125, 0, 255)
+    (155, 89, 182): (
+        "separator",
+        ImageRegionType,
+        "ImageRegion",
+        None,
+    ),  # separator, orginally (125, 125, 125)
+}
 
 
 # wrap Eynollah inference into an ocrd processor class
@@ -59,7 +107,8 @@ class EynollahInferenceProcessor(Processor):
     def shutdown(self) -> None:
         # TODO check if we need to close tensorflow session
         # But the session is store locally and not in the class
-        pass  # not really sure about this
+        if hasattr(self, "detector"):
+            del self.detector
 
     def process_page_pcgts(
         self, *input_pcgts: Optional[OcrdPage], page_id: Optional[str] = None
@@ -72,6 +121,7 @@ class EynollahInferenceProcessor(Processor):
         assert self.parameter  # default values or from CLI with -p or -P
 
         pcgts = input_pcgts[0]
+        result = OcrdPageResult(pcgts)
         page = pcgts.get_Page()
 
         # get the image file path for the page
@@ -103,8 +153,8 @@ class EynollahInferenceProcessor(Processor):
         # convert segmentation mask to PAGE regions
         self._add_regions_from_layout(page, only_layout)
 
-        # Add output PAGE-XML to workspace
-        file_id = self.output_file_grp + "_" + page_id
+        # add output PAGE-XML to workspace
+        file_id = layout_path.stem
 
         self.workspace.add_file(
             ID=file_id,
@@ -115,53 +165,96 @@ class EynollahInferenceProcessor(Processor):
             content=pcgts.to_xml(),
         )
 
-        return OcrdPageResult(pcgts)
+        # record alternative image with layout overlayed
+        alt_img = AlternativeImageType(
+            filename=os.path.join(self.output_file_grp, file_id + "_overlayed.png"),
+            comments="Eynollah inference result overlayed on original image",
+        )
+        result.pcgts.Page.add_AlternativeImage(alt_img)
 
-    def _add_regions_from_layout(self, page, layout_mask):
+        # also add the layout image as an OcrdPageResultImage
+        result.images.append(OcrdPageResultImage(only_layout, ".layout", alt_img))
+
+        return result
+
+    def _polygons_from_rgb_array(arr, skip_colors=[(0, 0, 0)]):
+        """Convert an RGB NumPy array into Shapely polygons grouped by RGB value."""
+
+        polygons_by_color = defaultdict(list)
+
+        # Get unique RGB values
+        colors = np.unique(arr.reshape(-1, 3), axis=0)
+
+        for color in colors:
+            color = tuple(int(c) for c in color)
+            if color in skip_colors:
+                continue
+
+            # Create mask for this RGB value
+            mask = np.all(arr == color, axis=-1)
+
+            # Extract contours
+            contours = measure.find_contours(mask.astype(np.uint8), level=0.5)
+
+            for contour in contours:
+                coords = [(float(c[1]), float(c[0])) for c in contour]
+
+                if len(coords) >= 3:
+                    poly = Polygon(coords)
+
+                    if poly.is_valid and poly.area > 0:
+                        polygons_by_color[color].append(poly)
+
+        # # Merge polygons per color
+        # for color, polys in polygons_by_color.items():
+        #     polygons_by_color[color] = unary_union(polys)
+
+        return polygons_by_color
+
+    def _add_regions_from_layout(
+        self, page: OcrdPage, layout_mask: np.ndarray, skip_colors=[(0, 0, 0)]
+    ) -> None:
         """Convert segmentation mask to PAGE regions."""
 
         layout_mask = layout_mask.astype(np.uint8)
 
-        labels = np.unique(layout_mask)
+        # get polygons from the layout mask
+        polygons_by_color = self._polygons_from_rgb_array(
+            layout_mask, skip_colors=skip_colors
+        )
 
-        region_counter = 0
+        region_idx = 0
+        for color, polygons in polygons_by_color.items():
+            info = eynollah_inference_colors.get(color, None)
 
-        for label in labels:
-
-            if label == 0:
-                continue  # background
-
-            binary = (layout_mask == label).astype(np.uint8)
-
-            contours, _ = cv2.findContours(
-                binary,
-                cv2.RETR_EXTERNAL,
-                cv2.CHAIN_APPROX_SIMPLE,
-            )
-
-            for contour in contours:
-
-                if cv2.contourArea(contour) < 200:
-                    continue
-
-                polygon = contour.squeeze()
-
-                if polygon.ndim != 2:
-                    continue
-
-                points = " ".join(f"{x},{y}" for x, y in polygon)
-
-                region = TextRegionType(
-                    id=f"region_{region_counter}",
-                    Coords=CoordsType(points=points),
+            if info is None:
+                self.logger.warning(
+                    "Color %s not found in the results of Eynollah inference, skipping",
+                    color,
                 )
+                continue
 
-                page.add_TextRegion(region)
+            label, region_type, region_label, subtype = info
 
-                region_counter += 1
+            if region_type is None:
+                continue  # skip background
+
+            for poly in polygons:
+                coords = CoordsType(points_from_polygon(poly))
+                region = region_type(
+                    id=f"region_{region_idx+1:04d}_{label}", Coords=coords, type=subtype
+                )
+                # add the region to the PAGE XML structure
+                getattr(page, f"add_{region_label}")(
+                    region
+                )  # e.g. page.add_TextRegion(region)
 
 
 @click.command()
 @ocrd_cli_options
 def cli(*args, **kwargs):
     return ocrd_cli_wrap_processor(EynollahInferenceProcessor, *args, **kwargs)
+
+
+if __name__ == "__main__":
+    cli()
